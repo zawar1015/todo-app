7e041ba3e426c79d73c57de64b99d2d547cf1a9a
^Zawar Hussain <zawarhussin.malak@gmial.com>
2025-11-24 01:13:15 +0500

    userInput validation feature added.

--- app.js
+++ app.js
@@ -7,33 +7,85 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 })
 
+function showError(whereToPlaceError , errorMessage){ 
+    let errorTag = document.getElementById(`${whereToPlaceError}`);
+    errorTag.textContent = errorMessage;
+    errorTag.style.display = "flex"
+}
 
-// function handleSignup(event){
-//     // e.preventDefault();
-
-//     console.log("zawar");
-// }
-
+function isValidEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+        
+}
+ 
 function handleSignUp(e) {
     e.preventDefault();
 
     const { name: { value: name }, email: { value: email }, password: { value: password } } = e.target;
+
+
+    //  name validation
+    if (name == "") {
+        showError("nameError" , "name should not be empyt")
+        return 
+    }
+    if (name.length < 3 || name.length > 20) {
+        showError("nameError" , "name should be greater than 3 and less than 20 characters")
+        return 
+    }
+
+    // email validation
+    if(!isValidEmail(email)){
+        showError("emailError" , "please enter a valid email")
+        return 
+    }
+
+     //  password validation
+    if (password == "") {
+        showError("passwordError" , "password should not be empyt")
+        return 
+    }
+    if (password.length < 8 || password.length > 20) {
+        showError("passwordError" , "password should be greater than 8 and less than 20 character")
+        return 
+    }
+    
     let newUser = {
         name,
         email,
         password
     }
-
     allUserData.push(newUser)
     currentUserData = newUser
 
     localStorage.setItem("allUsersData", JSON.stringify(allUserData))
     localStorage.setItem("currentUserData", JSON.stringify(currentUserData))
 
-
-
-
 }
-function handlelogin() {
+
+
+
+function handlelogin(e) {
+    e.preventDefault();
+
+    const {email: { value: email }, password: { value: password } } = e.target;
+
+
+    // email validation
+    if(!isValidEmail(email)){
+        showError("emailError" , "please enter a valid email")
+        return 
+    }
+
+     //  password validation
+    if (password == "") {
+        showError("passwordError" , "password should not be empyt")
+        return 
+    }
+    if (password.length < 8 || password.length > 20) {
+        showError("passwordError" , "password should be greater than 8 and less than 20 character")
+        return 
+    }
     
 }
